fcde0b4745bd90ccdc9a0495e59dc8c338717c6b
Luciano Ribeiro <luciano1025g@gmail.com>
2024-02-21 00:27:32 -0300

    commit inicial

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const form = document.getElementById('form-aatividade');
+const form = document.getElementById('form-atividade');
 const imgAprovado = 'img src="./images/aprovado.png" alt="Emoji celebrando" />';
 const imgReprovado = 'img src="./images/reprovado.png" alt="Emoji decepcionado" />';
 const atividade = [];
